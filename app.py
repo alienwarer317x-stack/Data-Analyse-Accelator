b2d862354c879759a016fe9c97ead882ea7a4883
alienwarer317x-stack <alienwarer317x@gmail.com>
2026-04-12 13:57:17 +1000

    app.py

--- app.py
+++ app.py
@@ -71,7 +71,8 @@ if client_mode == "DSR Upload":
         df = pd.read_excel(uploaded_file, sheet_name="Sheet1")
 
         discovered = []
-        st.session_state.discovery_df = pd.DataFrame(discovered)
+        st.session_state.discovery_df_dsr = pd.DataFrame(discovered)
+        st.session_state.discovery_df = st.session_state.discovery_df_dsr
 
         for _, r in df.iterrows():
             if selected_state != "All" and r.get("State") != selected_state:
